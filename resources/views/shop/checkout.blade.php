e717b6e55d46fee445572b8810df3395fa10c989
ShubhankarDas <shubh065@gmail.com>
2017-07-06 22:22:55 +0530

    Working

---
+++ resources/views/shop/checkout.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('title')
+Checkout
+@endsection
+@section('content')
+  <div class="row">
+    <div class="col-sm-6 col-md-6 col-sm-offset-3 col-md-offset-3">
+      <div class="thumbnail">
+        <h3>Login</h3>
+      </div>
+      <div class="thumbnail">
+        <h3>Login</h3>
+      </div>
+      <div class="thumbnail">
+        <h3>Login</h3>
+      </div>
+    </div>
+  </div>
+@endsection
